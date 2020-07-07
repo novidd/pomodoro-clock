8d9bdb4fcb4b2d69b601dc9b1fc990899443ce3a
Lucas Pettersson <clm.pettersson@gmail.com>
2020-07-07 23:12:50 +0200

    Updates

---
+++ precache-manifest.94eff4a017198f3e692801fd9ecbdf5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d777b662591647c66c678f5c30b4c6da",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "f588d17ccf8a314e21bd",
+    "url": "/pomodoro-clock/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f588d17ccf8a314e21bd",
+    "url": "/pomodoro-clock/static/js/main.493cb53b.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
